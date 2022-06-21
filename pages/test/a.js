fdaa3b3e98a79ef3bd71bc5124aa4e088fe3639a
Prasit.cpt <tempkaew@gmail.com>
2022-06-21 08:55:45 +0700

    test redirect

---
+++ pages/test/a.js
@@ -0,0 +1,20 @@
+function Page({}) {
+
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+// This gets called on every request
+export async function getServerSideProps({ req, query }) {
+    return {
+        redirect: {
+            permanent: false,
+            destination: 'https://hotnewsatth.blogspot.com/2019/10/gps.html'
+        }
+    }
+}
+
+export default Page
